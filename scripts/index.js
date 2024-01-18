f2676d4ae2d913bd8deff2d79ae1162d3df03675
zikara <danila.karavaev1@mail.ru>
2024-01-18 15:06:12 +0700

    works!!!

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
 
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list')
+
 // @todo: Функция создания карточки
 function createCard(cardData,deleteCard){
   const cardTemplate = document.querySelector('#card-template').content;
@@ -12,14 +13,14 @@ function createCard(cardData,deleteCard){
   cardImage.alt = cardData.name;
   cardElement.querySelector('.card__title').textContent = cardData.name;
 
-  cardElement.querySelector('.card__delete-button').addEventListener('click', function (){
-    deleteCard(cardElement);
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function (event){
+    const currentCard = event.currentTarget.closest('.card');
+    deleteCard(currentCard);
   })
   return cardElement;
 }
 // @todo: Функция удаления карточки
-function deleteCard(cardData) {
-  const cardElement = document.querySelector('.card'); 
+function deleteCard(cardElement) {
    cardElement.remove()
 }
 // @todo: Вывести карточки на страницу
