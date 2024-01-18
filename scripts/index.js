c7af6c5206cb819066dbcb464a85246f205f67dd
zikara <danila.karavaev1@mail.ru>
2024-01-18 07:40:58 +0700

    save

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list')
 // @todo: Функция создания карточки
-
+function createCard(cardData,deleteCard){
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function (){
+    deleteCard(cardElement);
+  })
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(cardData) {
+  const cardElement = document.querySelector('.card'); 
+  cardElement.parentNode.removeChild(cardElement);
+}
 // @todo: Вывести карточки на страницу
+function renderCards(cardsArray, deleteCard) {
+  cardsArray.forEach(function (card) {
+    const newCard = createCard(card, deleteCard); 
+    placesList.appendChild(newCard);
+  });
+}
+
+renderCards(initialCards, deleteCard);
