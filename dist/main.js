4e458ca2353291d4e8e3f97c9d255d019be8cf74
zikara <danila.karavaev1@mail.ru>
2024-02-18 21:46:09 +0700

    pered urok 10

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var n=[2,3,5].map((function(n){return 2*n}));console.log(n)})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,38 @@
-module.exports = {
-  entry: { main: './src/index.js' }
-}
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-// module.exports — это синтаксис экспорта в Node.js
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+        publicPath: ''
+  },
+    mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
+  },
+    module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      }
+      ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+  ]
+};
