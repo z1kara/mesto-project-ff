60b7768c534bdc1620dae751a337501832ef88b2
zikara <danila.karavaev1@mail.ru>
2024-02-18 22:49:20 +0700

    save

--- webpack.config.js
+++ webpack.config.js
@@ -4,55 +4,50 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-        publicPath: ''
+    publicPath: '',
   },
-    mode: 'development',
+  mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
+    open: true,
     compress: true,
-    port: 8080,
-    open: true
+    port: 8080
   },
-    module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
-      {
-        // применять это правило только к CSS-файлам
-        test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          // добавьте объект options
-          options: { importLoaders: 1 }
-        },
-          // Добавьте postcss-loader
-        'postcss-loader']
-      }, ,
-      {
-        // регулярное выражение, которое ищет все js файлы
+  module: {
+    rules: [{
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
       },
       {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
-      }
-      ]
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' // путь к файлу index.html
+      template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+
   ]
-};
+}
