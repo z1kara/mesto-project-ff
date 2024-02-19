400b48e464e09ea805ed8faa41ef165b17ef2c99
zikara <danila.karavaev1@mail.ru>
2024-02-19 21:17:20 +0700

    save

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 
 import "./pages/index.css";
+
+// const avatar = new URL('./images/avatar.jpg', import.meta.url);
+// // const jamesImage = new URL('./images/james.jpg', import.meta.url);
+// // const bryantImage = new URL('./images/bryant.jpg', import.meta.url)
+
+// const whoIsTheGoat = [
+//   // меняем исходные пути на переменные
+//   { name: 'page avatar', link: avatar },
+// ];
+
