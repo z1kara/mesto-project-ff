44e39e88fd76f8709e7c25c6f3b87edc9cab0a21
zikara <danila.karavaev1@mail.ru>
2024-02-23 22:20:52 +0700

    modal -> index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,11 +6,7 @@ import {deleteCard, createCard } from "./card"
 
 import {
   openPopup,
-  openImagePopup,
-  openEditProfilePopup,
-  handleFormSubmit,
-  handleNewCardSubmit,
-  
+  closePopup
 } from "./modal";
 
 // @todo: Темплейт карточки
@@ -31,6 +27,74 @@ const cardNameInput = newCardFormElement.querySelector(
 const cardLinkInput = newCardFormElement.querySelector(
   ".popup__input_type_url"
 );
+
+//edit profile
+function openEditProfilePopup() {
+  // Получите значения полей из соответствующих элементов на странице
+
+  const currentName = document.querySelector(".profile__title").textContent;
+  const currentDescription = document.querySelector(
+    ".profile__description"
+  ).textContent;
+
+  // Заполните поля в попапе текущими значениями
+  nameInput.value = currentName;
+  jobInput.value = currentDescription;
+
+  openPopup(".popup_type_edit");
+}
+
+//submit edit profile
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  const newName = nameInput.value;
+  const newJob = jobInput.value;
+  const profileTitle = document.querySelector(".profile__title");
+  const profileDescription = document.querySelector(".profile__description");
+
+  // Вставляем новые значения с помощью textContent
+  profileTitle.textContent = newName;
+  profileDescription.textContent = newJob;
+
+  closePopup(document.querySelector(".popup_type_edit"));
+}
+
+//Обработчик формы для новой карточки
+
+function handleNewCardSubmit(evt) {
+  evt.preventDefault();
+  const cardName = cardNameInput.value;
+  const cardLink = cardLinkInput.value;
+
+  const newCardData = {
+    name: cardName,
+    link: cardLink,
+  };
+
+  // Создаем новую карточку и добавляем её в начало контейнера
+  const newCardElement = createCard(newCardData, deleteCard, openImagePopup);
+  placesList.prepend(newCardElement);
+
+  // Очищаем форму
+  newCardFormElement.reset();
+
+  closePopup(document.querySelector(".popup_type_new-card"));
+}
+
+//открытие попапа с изображением
+
+function openImagePopup(imageUrl, captionText) {
+  const imagePopup = document.querySelector(".popup_type_image");
+  const imageElement = imagePopup.querySelector(".popup__image");
+  const captionElement = imagePopup.querySelector(".popup__caption");
+
+  imageElement.src = imageUrl;
+  imageElement.alt = "Image";
+  captionElement.textContent = captionText;
+  openPopup(".popup_type_image");
+}
 
 //вызов попапов
 document
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -44,78 +44,9 @@ function closePopup(popup) {
   document.removeEventListener("keydown", closePopup);
 }
 
-//edit profile
-function openEditProfilePopup() {
-  // Получите значения полей из соответствующих элементов на странице
 
-  const currentName = document.querySelector(".profile__title").textContent;
-  const currentDescription = document.querySelector(
-    ".profile__description"
-  ).textContent;
-
-  // Заполните поля в попапе текущими значениями
-  nameInput.value = currentName;
-  jobInput.value = currentDescription;
-
-  openPopup(".popup_type_edit");
-}
-
-//submit edit profile
-
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-
-  const newName = nameInput.value;
-  const newJob = jobInput.value;
-  const profileTitle = document.querySelector(".profile__title");
-  const profileDescription = document.querySelector(".profile__description");
-
-  // Вставляем новые значения с помощью textContent
-  profileTitle.textContent = newName;
-  profileDescription.textContent = newJob;
-
-  closePopup(document.querySelector(".popup_type_edit"));
-}
-
-//Обработчик формы для новой карточки
-
-function handleNewCardSubmit(evt) {
-  evt.preventDefault();
-  const cardName = cardNameInput.value;
-  const cardLink = cardLinkInput.value;
-
-  const newCardData = {
-    name: cardName,
-    link: cardLink,
-  };
-
-  // Создаем новую карточку и добавляем её в начало контейнера
-  const newCardElement = createCard(newCardData, deleteCard, openImagePopup);
-  placesList.prepend(newCardElement);
-
-  // Очищаем форму
-  newCardFormElement.reset();
-
-  closePopup(document.querySelector(".popup_type_new-card"));
-}
-
-//открытие попапа с изображением
-
-function openImagePopup(imageUrl, captionText) {
-  const imagePopup = document.querySelector(".popup_type_image");
-  const imageElement = imagePopup.querySelector(".popup__image");
-  const captionElement = imagePopup.querySelector(".popup__caption");
-
-  imageElement.src = imageUrl;
-  imageElement.alt = "Image";
-  captionElement.textContent = captionText;
-  openPopup(".popup_type_image");
-}
 
 export {
   openPopup,
-  openEditProfilePopup,
-  handleFormSubmit,
-  handleNewCardSubmit,
-  openImagePopup,
+  closePopup
 };
